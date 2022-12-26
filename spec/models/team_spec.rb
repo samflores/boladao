97da0614065bf254f3902fdb3d96ae570d70c37d
Samuel Flores <me@samflor.es>
2022-12-26 15:39:59 -0300

    Add controller to display mock teams index and show pages

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,56 +1,58 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Team, type: :model do
+RSpec.describe Team do
   it 'requires name to be present' do
-    team = Team.new(name: nil, short_name: 'BRA')
+    team = described_class.new(name: nil, short_name: 'BRA')
 
-    expect(team).to_not be_valid
+    expect(team).not_to be_valid
     expect(team.errors[:name]).to include("can't be blank")
   end
 
   it 'requires name to be filled' do
-    team = Team.new(name: '', short_name: 'BRA')
+    team = described_class.new(name: '', short_name: 'BRA')
 
-    expect(team).to_not be_valid
+    expect(team).not_to be_valid
     expect(team.errors[:name]).to include("can't be blank")
   end
 
   it 'requires name to be unique' do
-    Team.create(name: 'Brazil', short_name: 'BRA')
+    described_class.create(name: 'Brazil', short_name: 'BRA')
 
-    team = Team.new(name: 'Brazil', short_name: 'BRL')
+    team = described_class.new(name: 'Brazil', short_name: 'BRL')
 
-    expect(team).to_not be_valid
+    expect(team).not_to be_valid
     expect(team.errors[:name]).to include('has already been taken')
   end
 
   it 'requires short name to be present' do
-    team = Team.new(name: 'Brazil', short_name: nil)
+    team = described_class.new(name: 'Brazil', short_name: nil)
 
-    expect(team).to_not be_valid
+    expect(team).not_to be_valid
     expect(team.errors[:short_name]).to include("can't be blank")
   end
 
   it 'requires short name to be filled' do
-    team = Team.new(name: 'Brazil', short_name: '')
+    team = described_class.new(name: 'Brazil', short_name: '')
 
-    expect(team).to_not be_valid
+    expect(team).not_to be_valid
     expect(team.errors[:short_name]).to include("can't be blank")
   end
 
   it 'requires short name to be unique' do
-    Team.create(name: 'Brazil', short_name: 'BRA')
+    described_class.create(name: 'Brazil', short_name: 'BRA')
 
-    team = Team.new(name: 'Brasil', short_name: 'BRA')
+    team = described_class.new(name: 'Brasil', short_name: 'BRA')
 
-    expect(team).to_not be_valid
+    expect(team).not_to be_valid
     expect(team.errors[:short_name]).to include('has already been taken')
   end
 
   it 'requires short name to have 3 characters' do
-    team = Team.new(name: 'Brazil', short_name: 'BRAZIL')
+    team = described_class.new(name: 'Brazil', short_name: 'BRAZIL')
 
-    expect(team).to_not be_valid
+    expect(team).not_to be_valid
     expect(team.errors[:short_name]).to include('is the wrong length (should be 3 characters)')
   end
 end

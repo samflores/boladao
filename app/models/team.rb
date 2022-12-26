00046dcd1a0721d15f36bb185045903a77da3eb4
Samuel Flores <me@samflor.es>
2022-12-26 16:55:51 -0300

    Fix pagination scope

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
+  PAGE_SIZE = 12
+
   validates :name,
             presence: true,
             uniqueness: true
@@ -9,7 +11,7 @@ class Team < ApplicationRecord
             uniqueness: true,
             length: { is: 3 }
 
-  scope :in_page, ->(page) { offset((page.to_i - 1) * 12) if page }
+  scope :in_page, ->(page) { offset((page.to_i - 1) * PAGE_SIZE).limit(PAGE_SIZE) if page }
   scope :with_short_name, ->(short_name) { where(short_name:) if short_name }
 
   def to_param

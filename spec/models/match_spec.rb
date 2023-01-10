60922d6ce22d9bd1fa10879b9bd9d886419edf46
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-01-09 21:08:51 -0300

    test: write spec/models/match_spec.rb

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:tournament) }
+    it { is_expected.to belong_to(:team_a).class_name('Team') }
+    it { is_expected.to belong_to(:team_b).class_name('Team') }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:starts_at) }
+    it { is_expected.to validate_presence_of(:tournament) }
+    it { is_expected.to validate_presence_of(:team_a) }
+    it { is_expected.to validate_presence_of(:team_b) }
+  end
+
+  describe 'enums' do
+    it { is_expected.to define_enum_for(:phase).with_prefix }
+  end
 end

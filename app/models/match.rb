3cf14bc72844a090a04631ae7fa9f160ba35b22e
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-01-10 22:15:24 -0300

    chore: add team_a_cannot_be_equal_to_team_b validation

--- app/models/match.rb
+++ app/models/match.rb
@@ -15,5 +15,12 @@ class Match < ApplicationRecord
   validates :tournament, presence: true
   validates :team_a, presence: true
   validates :team_b, presence: true
-  # TODO: validate team_a and team_b to be different
+  validate :team_a_cannot_be_equal_to_team_b
+
+  def team_a_cannot_be_equal_to_team_b
+    if team_a_id == team_b_id
+      errors.add(:team_a, "can't be equal to team b")
+      errors.add(:team_b, "can't be equal to team a")
+    end
+  end
 end

e86509c45825bad3e9c8290e6810d95c2110d4e3
Mateus Wilding <mateus.wilding@codeminer42.com>
2023-04-11 11:18:13 -0300

    Adds user registration and signin

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
+  def create
+    super
+  end
+
+  private 
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end

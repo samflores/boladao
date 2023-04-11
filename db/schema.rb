7c1de32080a17d91956defaaab57f59de4c1ffe9
Mateus Wilding <mateus.wilding@codeminer42.com>
2023-04-11 11:19:00 -0300

    database updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_202000) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_163341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_202000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 1, null: false
+    t.string "name", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
